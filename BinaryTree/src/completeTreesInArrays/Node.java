caef7ed5d543d609d8a7cf2ad29d19f1cffd2593
heshi-hub <heshinijayaweera@gmail.com>
2026-03-06 10:42:45 +0100

    Add files via upload

---
+++ BinaryTree/src/completeTreesInArrays/Node.java
@@ -0,0 +1,15 @@
+package completeTreesInArrays;
+
+public class Node {
+	
+	private int value;
+	
+	public Node(int value) {
+		this.value = value;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+}
